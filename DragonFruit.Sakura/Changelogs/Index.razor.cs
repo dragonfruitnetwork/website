ac2a16a4f3edcad39cda63a0362d3506a8fe51e5
Albie <albion@dragonfruit.network>
2022-06-22 16:07:18 +0100

    add main layout

---
+++ DragonFruit.Sakura/Changelogs/Index.razor.cs
@@ -0,0 +1,27 @@
+﻿using DragonFruit.Data;
+using DragonFruit.Sakura.Network;
+using DragonFruit.Sakura.Network.Requests;
+using Microsoft.AspNetCore.Components;
+
+namespace DragonFruit.Sakura.Changelogs
+{
+    public partial class Index
+    {
+        private ApiChangelogRelease Release { get; set; }
+        
+        [Inject]
+        private ApiClient Client { get; set; }
+        
+        [Parameter]
+        public string AppName { get; set; }
+        
+        [Parameter]
+        public string VersionName { get; set; }
+        
+        protected override async Task OnParametersSetAsync()
+        {
+            var releaseRequest = new ApiChangelogsRequest(AppName, VersionName);
+            Release = await Client.PerformAsync<ApiChangelogRelease>(releaseRequest).ConfigureAwait(false);
+        }
+    }
+}
