ddf7635a59bffb16622f7a3fef0ffb5f8e844c6b
Albie <albion@dragonfruit.network>
2022-06-22 17:49:09 +0100

    add app name property

--- DragonFruit.Sakura/Network/ApiChangelogRelease.cs
+++ DragonFruit.Sakura/Network/ApiChangelogRelease.cs
@@ -10,19 +10,22 @@ namespace DragonFruit.Sakura.Network
 
         [JsonPropertyName("target_app")]
         public string AppId { get; set; }
-        
+
+        [JsonPropertyName("app_name")]
+        public string AppName { get; set; }
+
         [JsonPropertyName("released")]
         public DateTime Release { get; set; }
 
         [JsonPropertyName("version_name")]
         public string VersionName { get; set; }
-        
+
         [JsonPropertyName("next_id")]
         public string NextRelease { get; set; }
 
         [JsonPropertyName("prev_id")]
         public string PreviousRelease { get; set; }
-        
+
         [JsonPropertyName("modifications")]
         public IReadOnlyCollection<ApiChangelogModification> Modifications { get; set; }
     }
