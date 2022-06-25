89b3ae9c0ea0026d9776e03ba5989226de8cd29c
Albie <albion@dragonfruit.network>
2022-06-25 12:53:01 +0100

    add country list template

---
+++ DragonFruit.Sakura/OnionFruit/Countries.razor.cs
@@ -0,0 +1,23 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using DragonFruit.Data;
+using DragonFruit.Sakura.Network;
+using DragonFruit.Sakura.Network.Requests;
+using Microsoft.AspNetCore.Components;
+
+namespace DragonFruit.Sakura.OnionFruit
+{
+    public partial class Countries
+    {
+        [Inject]
+        private ApiClient Client { get; set; }
+
+        private IReadOnlyCollection<OnionCountryInfo> NodeCountries { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            NodeCountries = await Client.PerformAsync<IReadOnlyCollection<OnionCountryInfo>>(new OnionCountryInfoRequest()).ConfigureAwait(false);
+        }
+    }
+}
