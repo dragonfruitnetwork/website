0d75814876f365cfb4e80d682b1bd9226c76b2b0
Albie <albion@dragonfruit.network>
2022-06-25 12:51:38 +0100

    add country info api and responses

---
+++ DragonFruit.Sakura/Network/Requests/OnionCountryInfoRequest.cs
@@ -0,0 +1,12 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using DragonFruit.Data;
+
+namespace DragonFruit.Sakura.Network.Requests
+{
+    public class OnionCountryInfoRequest : ApiRequest
+    {
+        public override string Path => "https://onioncdn.dragonfruit.network/country-details.json";
+    }
+}
