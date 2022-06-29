f698d92430870e08fca1e64ab0d3d0affec3294c
Albie <albion@dragonfruit.network>
2022-06-29 15:10:06 +0100

    add wiki api items

---
+++ DragonFruit.Sakura/Network/ApiWikiPage.cs
@@ -0,0 +1,16 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using System.Text.Json.Serialization;
+
+namespace DragonFruit.Sakura.Network
+{
+    public class ApiWikiPage
+    {
+        [JsonPropertyName("path")]
+        public string Path { get; set; }
+
+        [JsonPropertyName("page_content")]
+        public string PageContent { get; set; }
+    }
+}
---
+++ DragonFruit.Sakura/Network/Requests/ApiWikiPageRequest.cs
@@ -0,0 +1,19 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using DragonFruit.Data;
+
+namespace DragonFruit.Sakura.Network.Requests
+{
+    public class ApiWikiPageRequest : ApiRequest
+    {
+        public override string Path => $"https://dragonfruit.network/api/wiki/{PagePath.TrimStart('/')}";
+
+        public ApiWikiPageRequest(string pagePath)
+        {
+            PagePath = pagePath;
+        }
+
+        public string PagePath { get; }
+    }
+}
