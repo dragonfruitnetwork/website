a93cf2c44b1b3d37d8506291047760bb9decaa68
Albie <albion@dragonfruit.network>
2022-06-29 15:10:52 +0100

    add wiki page and wiki renderer/processor service

---
+++ DragonFruit.Sakura/Wiki/WikiPage.cs
@@ -0,0 +1,19 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using Microsoft.AspNetCore.Components;
+
+namespace DragonFruit.Sakura.Wiki
+{
+    public class WikiPage
+    {
+        public string Title { get; set; }
+        public string PagePath { get; set; }
+
+        public bool Stub { get; set; }
+        public bool Outdated { get; set; }
+        public IReadOnlyCollection<string> Tags { get; set; }
+
+        public MarkupString Content { get; set; }
+    }
+}
---
+++ DragonFruit.Sakura/Wiki/WikiRenderer.cs
@@ -0,0 +1,55 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using System.Text.RegularExpressions;
+using DragonFruit.Sakura.Network;
+using DragonFruit.Sakura.Wiki.Renderers;
+using Markdig;
+using Microsoft.AspNetCore.Components;
+using SharpYaml.Serialization;
+
+namespace DragonFruit.Sakura.Wiki
+{
+    public class WikiRenderer
+    {
+        private readonly MarkdownPipeline _pipeline;
+        private readonly Serializer _yamlSerializer;
+
+        private readonly Regex _pageFormat = new(@"^(?:---\r?\n(?<header>.*)---(?:\r?\n)+)?(?<document>.+)$", RegexOptions.Compiled | RegexOptions.Singleline);
+        private readonly Regex _pageHeader = new(@"(?:#+ (.*))", RegexOptions.Compiled);
+
+        public WikiRenderer()
+        {
+            _pipeline = new MarkdownPipelineBuilder()
+                        .UseAdvancedExtensions()
+                        .UseBootstrap()
+                        .Use<AlertQuoteBlockRendererExtension>()
+                        .Build();
+
+            _yamlSerializer = new Serializer();
+            _yamlSerializer.Settings.NamingConvention = new FlatNamingConvention();
+        }
+
+        /// <summary>
+        /// Processes a <see cref="ApiWikiPage"/>, converting it into a usable <see cref="WikiPage"/>
+        /// </summary>
+        public WikiPage ProcessPage(ApiWikiPage apiPage)
+        {
+            var pageStructure = _pageFormat.Match(apiPage.PageContent);
+
+            // process to get header
+            var header = pageStructure.Groups["header"];
+            var page = header.Success ? _yamlSerializer.Deserialize<WikiPage>(header.Value) : new WikiPage();
+
+            // then get the content
+            var rawContent = pageStructure.Groups["document"].Value;
+            var titleCapture = _pageHeader.Match(rawContent).Groups[1];
+
+            page.PagePath = apiPage.Path;
+            page.Title = titleCapture.Success ? titleCapture.Value : "Wiki";
+            page.Content = (MarkupString)Markdown.ToHtml(rawContent, _pipeline);
+
+            return page;
+        }
+    }
+}
