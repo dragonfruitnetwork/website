6848bfe601174d445206b22e6a61f6a2fc6af260
Albie <albion@dragonfruit.network>
2022-07-04 18:41:12 +0100

    add link renderer (image processor)

---
+++ DragonFruit.Sakura/Wiki/Renderers/Extensions/SakuraLinkRendererExtension.cs
@@ -0,0 +1,23 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using Markdig;
+using Markdig.Renderers;
+using Markdig.Renderers.Html.Inlines;
+
+namespace DragonFruit.Sakura.Wiki.Renderers.Extensions
+{
+    public class SakuraLinkRendererExtension : IMarkdownExtension
+    {
+        public void Setup(MarkdownPipelineBuilder pipeline)
+        {
+            // do nothing
+        }
+
+        public void Setup(MarkdownPipeline pipeline, IMarkdownRenderer renderer)
+        {
+            // swap the built-in renderer with our own
+            renderer.ObjectRenderers.Replace<LinkInlineRenderer>(new SakuraLinkInlineRenderer());
+        }
+    }
+}
---
+++ DragonFruit.Sakura/Wiki/Renderers/SakuraLinkInlineRenderer.cs
@@ -0,0 +1,25 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using System.Text.RegularExpressions;
+using Markdig.Renderers;
+using Markdig.Renderers.Html.Inlines;
+using Markdig.Syntax.Inlines;
+
+namespace DragonFruit.Sakura.Wiki.Renderers
+{
+    public class SakuraLinkInlineRenderer : LinkInlineRenderer
+    {
+        protected override void Write(HtmlRenderer renderer, LinkInline link)
+        {
+            if (link.IsImage && (link.Url!.StartsWith("./") || link.Url.StartsWith('/')))
+            {
+                // replace image url with fully qualified one
+                var path = Regex.Replace(link.Url, @"^\.?\/", string.Empty);
+                link.Url = $"https://raw.githubusercontent.com/dragonfruitnetwork/wiki/master/{path}";
+            }
+
+            base.Write(renderer, link);
+        }
+    }
+}
