0a0e2c3ed33d601aee65ee5e9172f975171b867d
Albie <albion@dragonfruit.network>
2022-07-06 20:07:01 +0100

    add serilog configuration

--- DragonFruit.Sakura/Program.cs
+++ DragonFruit.Sakura/Program.cs
@@ -1,6 +1,7 @@
 // DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
 // Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
 
+using System.Reflection;
 using DragonFruit.Data;
 using DragonFruit.Data.Serializers.SystemJson;
 using DragonFruit.Sakura.Wiki;
@@ -8,6 +9,9 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor;
 using MudBlazor.Services;
+using Serilog;
+using Serilog.Events;
+using Serilog.Extensions.Logging;
 
 namespace DragonFruit.Sakura
 {
@@ -34,6 +38,23 @@ namespace DragonFruit.Sakura
             builder.Services.AddMudServices();
             builder.Services.AddSingleton<WikiRenderer>();
             builder.Services.AddSingleton<ApiClient, ApiClient<ApiSystemTextJsonSerializer>>();
+
+            var loggingConfig = new LoggerConfiguration()
+                                .MinimumLevel.Debug()
+                                .Enrich.WithProperty("InstanceId", Guid.NewGuid().ToString("D"))
+                                .WriteTo.BrowserConsole()
+                                .WriteTo.Sentry(o =>
+                                {
+                                    var version = Assembly.GetExecutingAssembly().GetName().Version;
+
+                                    o.MaxBreadcrumbs = 50;
+                                    o.MinimumEventLevel = LogEventLevel.Error;
+                                    o.MinimumBreadcrumbLevel = LogEventLevel.Debug;
+                                    o.Release = version?.ToString(version.Build > 0 ? 3 : 2);
+                                    o.Dsn = "https://d42ddda84a6f4ffb8d9d66cb7d1a6d9b@o97031.ingest.sentry.io/6542291";
+                                });
+
+            builder.Logging.AddProvider(new SerilogLoggerProvider(loggingConfig.CreateLogger(), true));
 
             await builder.Build().RunAsync();
         }
