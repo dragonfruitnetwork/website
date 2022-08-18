7716df40e6de795d8f1e90d2a24bf7aa586e5717
Albie <albion@dragonfruit.network>
2022-08-18 20:09:35 +0100

    move flag url generation to utility class

---
+++ DragonFruit.Sakura/Utils/FlagUtils.cs
@@ -0,0 +1,10 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+namespace DragonFruit.Sakura.Utils
+{
+    public class FlagUtils
+    {
+        public static string GetFlagUrl(string countryCode) => $"https://raw.githubusercontent.com/ppy/osu-resources/master/osu.Game.Resources/Textures/Flags/{countryCode}.png";
+    }
+}
