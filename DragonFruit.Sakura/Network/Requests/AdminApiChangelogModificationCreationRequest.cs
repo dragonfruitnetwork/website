0bbbc91196b042268af1f66231488c8560c00d1b
Albie <albion@dragonfruit.network>
2022-09-18 18:55:12 +0100

    add changelog modification change requests

---
+++ DragonFruit.Sakura/Network/Requests/AdminApiChangelogModificationCreationRequest.cs
@@ -0,0 +1,30 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using DragonFruit.Data;
+using DragonFruit.Data.Parameters;
+
+namespace DragonFruit.Sakura.Network.Requests
+{
+    public class AdminApiChangelogModificationCreationRequest : YunaApiRequest
+    {
+        protected override string Stub => $"/{AppId}/changelogs/{Version}/modifications";
+        protected override bool RequireAuth => true;
+
+        protected override Methods Method => Methods.Post;
+        protected override BodyType BodyType => BodyType.SerializedProperty;
+
+        public AdminApiChangelogModificationCreationRequest(string appId, string version, ApiChangelogModification modification)
+        {
+            AppId = appId;
+            Version = version;
+            Modification = modification;
+        }
+
+        public string AppId { get; }
+        public string Version { get; }
+
+        [RequestBody]
+        public ApiChangelogModification Modification { get; }
+    }
+}
---
+++ DragonFruit.Sakura/Network/Requests/AdminApiChangelogModificationDeletionRequest.cs
@@ -0,0 +1,26 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using DragonFruit.Data;
+
+namespace DragonFruit.Sakura.Network.Requests
+{
+    public class AdminApiChangelogModificationDeletionRequest : YunaApiRequest
+    {
+        protected override string Stub => $"/{AppId}/changelogs/{Version}/modifications/{ModificationId}";
+        protected override bool RequireAuth => true;
+
+        protected override Methods Method => Methods.Delete;
+
+        public AdminApiChangelogModificationDeletionRequest(string appId, string version, int modificationId)
+        {
+            AppId = appId;
+            Version = version;
+            ModificationId = modificationId;
+        }
+
+        public string AppId { get; }
+        public string Version { get; }
+        public int ModificationId { get; }
+    }
+}
