a18b4cad7ed417629a0bd27d92814101a51be159
Albie <albion@dragonfruit.network>
2022-09-18 11:55:09 +0100

    add changelogs api requests

---
+++ DragonFruit.Sakura/Network/Requests/AdminApiChangelogVersionListingRequest.cs
@@ -0,0 +1,18 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+namespace DragonFruit.Sakura.Network.Requests
+{
+    public class AdminApiChangelogVersionListingRequest : YunaApiRequest
+    {
+        protected override string Stub => $"/{AppId}/changelogs/list";
+        protected override bool RequireAuth => true;
+
+        public AdminApiChangelogVersionListingRequest(string appId)
+        {
+            AppId = appId;
+        }
+
+        public string AppId { get; }
+    }
+}
---
+++ DragonFruit.Sakura/Network/Requests/AdminApiChangelogsDeletionRequest.cs
@@ -0,0 +1,24 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using DragonFruit.Data;
+
+namespace DragonFruit.Sakura.Network.Requests
+{
+    public class AdminApiChangelogsDeletionRequest : YunaApiRequest
+    {
+        protected override string Stub => $"/{AppId}/changelogs/{VersionName}";
+        protected override bool RequireAuth => true;
+
+        protected override Methods Method => Methods.Delete;
+
+        public AdminApiChangelogsDeletionRequest(string appId, string versionName)
+        {
+            AppId = appId;
+            VersionName = versionName;
+        }
+
+        public string AppId { get; }
+        public string VersionName { get; }
+    }
+}
