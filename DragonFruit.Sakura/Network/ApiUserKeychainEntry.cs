599b143888e7325a10f5dc7b8013d08fbee93f07
Albie <albion@dragonfruit.network>
2022-09-20 19:32:53 +0100

    add keychain entry model

---
+++ DragonFruit.Sakura/Network/ApiUserKeychainEntry.cs
@@ -0,0 +1,19 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using System.Text.Json.Serialization;
+
+namespace DragonFruit.Sakura.Network
+{
+    public class ApiUserKeychainEntry
+    {
+        [JsonPropertyName("jwk_fingerprint")]
+        public string KeyId { get; set; }
+
+        [JsonPropertyName("created_utc")]
+        public DateTime CreatedUtc { get; set; }
+
+        [JsonPropertyName("expiry_utc")]
+        public DateTime? ExpiresUtc { get; set; }
+    }
+}
