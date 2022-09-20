8cd28d9f7f88a711a8b2c3096805dee0263ed897
Albie <albion@dragonfruit.network>
2022-09-20 17:26:47 +0100

    add keychain requests

---
+++ DragonFruit.Sakura/Network/Requests/AdminApiKeychainDeletionRequest.cs
@@ -0,0 +1,22 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using DragonFruit.Data;
+
+namespace DragonFruit.Sakura.Network.Requests
+{
+    public class AdminApiKeychainDeletionRequest : YunaApiRequest
+    {
+        protected override string Stub => $"/users/me/keychain/{KeyId}";
+        protected override bool RequireAuth => true;
+
+        protected override Methods Method => Methods.Delete;
+
+        public AdminApiKeychainDeletionRequest(string keyId)
+        {
+            KeyId = keyId;
+        }
+
+        public string KeyId { get; }
+    }
+}
