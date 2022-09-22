b2706fbc9f96cc70b95f2614ec610f67c06afaaf
Albie <albion@dragonfruit.network>
2022-09-22 10:22:56 +0100

    add branch model

---
+++ DragonFruit.Sakura/Network/ApiDistributionBranch.cs
@@ -0,0 +1,22 @@
+﻿// DragonFruit Sakura Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using System.Text.Json.Serialization;
+
+namespace DragonFruit.Sakura.Network
+{
+    public class ApiDistributionBranch
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("app_id")]
+        public string AppId { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("active_release_id")]
+        public int ActiveReleaseId { get; set; }
+    }
+}
