01ec7dfad17463ccf5c1029c4264130a0b1bf8e8
Albie <albion@dragonfruit.network>
2023-05-20 20:23:19 +0100

    add url override via YUNA_API_URL

--- DragonFruit.Sakura.Host/SakuraServerClient.cs
+++ DragonFruit.Sakura.Host/SakuraServerClient.cs
@@ -4,15 +4,22 @@
 using System.Net;
 using System.Net.Http;
 using DragonFruit.Sakura.Network;
+using Microsoft.Extensions.Configuration;
 
 namespace DragonFruit.Sakura.Host
 {
     public class SakuraServerClient : SakuraClient
     {
+        private readonly string _yunaUrl;
+
         public override bool IsServerSide => true;
 
-        public SakuraServerClient()
+        protected override string ApiBaseUrl => _yunaUrl ?? base.ApiBaseUrl;
+
+        public SakuraServerClient(IConfiguration config)
         {
+            _yunaUrl = config["YUNA_API_URL"];
+
             UserAgent = "Sakura-Server";
             Handler = () => new SocketsHttpHandler
             {
