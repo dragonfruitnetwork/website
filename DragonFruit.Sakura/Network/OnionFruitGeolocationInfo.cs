c69209ba782d011f11a407e02796e4dbd689b780
Albie Spriddell <albion@dragonfruit.network>
2023-12-19 13:54:28 +0000

    update geolocation response

--- DragonFruit.Sakura/Network/OnionFruitGeolocationInfo.cs
+++ DragonFruit.Sakura/Network/OnionFruitGeolocationInfo.cs
@@ -5,13 +5,22 @@ using System.Text.Json.Serialization;
 
 namespace DragonFruit.Sakura.Network
 {
-    public class ApiGeolocationInfo
+    public class OnionFruitGeolocationInfo
     {
         [JsonPropertyName("ip_address")]
         public string IPAddress { get; set; }
 
+        [JsonPropertyName("country_code")]
+        public string CountryCode { get; set; }
+
         [JsonPropertyName("country_name")]
-        public string Country { get; set; }
+        public string CountryName { get; set; }
+
+        [JsonPropertyName("as_number")]
+        public int? AS { get; set; }
+
+        [JsonPropertyName("as_name")]
+        public string ASName { get; set; }
 
         [JsonPropertyName("is_tor")]
         public bool? IsConnectedToTor { get; set; }
