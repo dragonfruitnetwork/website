1e4a663a3139b536fe538d8da8e5df743aff922d
Albie Spriddell <albion@dragonfruit.network>
2024-12-29 16:32:50 +0000

    use suspense when loading apps

--- app/changelogs/[[...slug]]/listing.tsx
+++ app/changelogs/[[...slug]]/listing.tsx
@@ -1,4 +1,5 @@
 import {prisma} from "@/prisma";
+import {Suspense, use} from "react";
 
 interface AppReleaseInfo {
     id: number;
@@ -7,8 +8,8 @@ interface AppReleaseInfo {
     releaseName: string;
 }
 
-export default async function Listing() {
-    const publicApps: AppReleaseInfo[] = await prisma.$queryRaw`
+export default function Listing() {
+    const publicApps: AppReleaseInfo[] = use(prisma.$queryRaw`
         SELECT app.id, app.name, app.color, cr.releaseName
         FROM changelog_apps app
                  JOIN (SELECT appId, MAX(releaseDate) as latestReleaseDate
@@ -17,17 +18,21 @@ export default async function Listing() {
                  JOIN changelog_releases cr
                       ON cr.appId = latestReleases.appId AND cr.releaseDate = latestReleases.latestReleaseDate
         WHERE app.public = true;
-    `;
+    `);
 
     return (
-        <div className="flex flex-row flex-wrap" style={{maxWidth: "100%", width: "max-content"}}>
-            {publicApps.map((x) => (
-                <a key={x.id} href={`/changelogs/${x.id}/${x.releaseName}`} className="block p-1 text-decoration-none w-[125px] hover:w-[150px] lg:hover:w-[175px] transition-all select-none" style={{willChange: "opacity"}}>
-                    <div className="rounded-full block h-[4px] mb-1 w-100" style={{backgroundColor: x.color}}/>
-                    <h6 className="text-xs font-semibold">{x.name}</h6>
-                    <h4 className="text-md">{x.releaseName}</h4>
-                </a>
-            ))}
-        </div>
+        <Suspense>
+            <div className="flex flex-row flex-wrap" style={{maxWidth: "100%", width: "max-content"}}>
+                {publicApps.map((x) => (
+                    <a key={x.id} href={`/changelogs/${x.id}/${x.releaseName}`}
+                       className="block p-1 text-decoration-none w-[125px] hover:w-[150px] lg:hover:w-[175px] transition-all select-none"
+                       style={{willChange: "opacity"}}>
+                        <div className="rounded-full block h-[4px] mb-1 w-100" style={{backgroundColor: x.color}}/>
+                        <h6 className="text-xs font-semibold">{x.name}</h6>
+                        <h4 className="text-md">{x.releaseName}</h4>
+                    </a>
+                ))}
+            </div>
+        </Suspense>
     );
 }
